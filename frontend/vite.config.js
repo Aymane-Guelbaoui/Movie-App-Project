5406e7878584e2c4fa65097d730fa138648b7b6d
AymaneGl <ay.baoui2016@gmail.com>
2025-10-06 21:58:59 +0100

    fix Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // important: makes asset paths relative for Vercel
+  base: './', // important for Vercel relative paths
   build: {
     outDir: 'dist', // Vercel serves this folder
-  },
+    emptyOutDir: true
+  }
 })
